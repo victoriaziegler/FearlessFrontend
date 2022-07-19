b457cddfae7088fd577a31bd13828db8e4c027da
Victoria Ziegler <toriziegler@cox.net>
2022-07-19 12:47:34 -0700

    One conference name, detail, image showing

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,33 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
+    const url = 'http://localhost:8000/api/conferences/'
 
+    try {
+        const response = await fetch(url)
+        if (!response.ok) {
+        } else {
+            const data = await response.json()
 
+            const conference = data.conferences[0]
+            
+            const nameTag = document.querySelector('.card-title')
+            nameTag.innerHTML = conference.name
+            
+            
+            const detailUrl = `http://localhost:8000${conference.href}`
+            const detailResponse = await fetch(detailUrl)
+            
+            if (detailResponse.ok) {
+                const details = await detailResponse.json()
+                
+                const descriptionTag = document.querySelector('.card-text')
+                descriptionTag.innerHTML = details.conference.description
+                
+                const imageTag = document.querySelector('.card-img-top')
+                imageTag.src = details.conference.location.picture_url;
+
+                console.log(details)
+            }
+        }
+        } catch (e) {
+        }
 });
