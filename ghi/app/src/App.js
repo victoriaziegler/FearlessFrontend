6fcd2bdc2c97b6924000ce98da8a08adafa42f89
Victoria Ziegler <toriziegler@cox.net>
2022-07-25 12:04:34 -0700

    Showing A Form complete

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Nav from './Nav'
+import AttendeesList from './AttendeesList';
 
 function App(props) {
   if (props.attendees === undefined) {
@@ -9,24 +10,7 @@ function App(props) {
     <React.Fragment>
     <Nav />
     <div className="container">
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Conference</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.attendees.map(attendee => {
-            return (
-              <tr key={attendee.href}>
-                <td>{ attendee.name }</td>
-                <td>{ attendee.conference }</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <AttendeesList attendees={props.attendees} />
     </div>
     </React.Fragment>
   );
